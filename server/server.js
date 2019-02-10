cbbb40d5fe1bf6539563e9613835b957845489d0
Avneesh Rai <avneesh.rai@jda.com>
2019-02-10 09:40:33 +0530

    Added code to Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'abc1',
-    text: 'How are you',
-    createdAt: 1231
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('Create message', newMessage);
+  socket.on('createMessage', (message) => {
+    console.log('Create message', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', ()=>{
